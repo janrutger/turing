b86e08cb40f5fd01a891acd5be1582ef3e129d78
janrutger <janrutger@jaruku.nl>
2020-05-12 21:30:11 +0200

    json load en dump (Via website gegenereerde json (part7)

--- auto-json.py
+++ auto-json.py
@@ -1,29 +1,31 @@
 {
   "command.com": {
-    "Commdando": "LDA",
-    "microcode": {
-      "status": "S0",
-      "waardes": {
-        "huidigewaardes": {
-          "ST": "1",
-          "RA": "1",
-          "RB": "1",
-          "S": "1"
-        },
-        "Nieuwewaardes": {
-          "ST": "0",
-          "RA": "0",
-          "RB": "0",
-          "S": "0"
-        },
-        "MOVES": {
-          "ST": "L",
-          "RA": "R",
-          "RB": "L",
-          "S": "S"
-        },
-        "nieuwestatus": {
-          "status": "S0"
+    "LDA": {
+      "S0": {
+        "microcode": {
+          "huidigewaardes": {
+            "ST": "1",
+            "RA": "1",
+            "RB": "1",
+            "S": "1"
+          },
+          "nieuwewaardes": {
+            "MOVES": {
+              "ST": "L",
+              "RA": "R",
+              "RB": "L",
+              "S": "S"
+            },
+            "Nieuwewaardes": {
+              "ST": "0",
+              "RA": "0",
+              "RB": "0",
+              "S": "0"
+            },
+            "nieuwestatus": {
+              "status": "S0"
+            }
+          }
         }
       }
     }
