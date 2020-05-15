7fd1d89d2789de46976171d267c5e68e007d5218
janrutger <janrutger@jaruku.nl>
2020-05-15 12:33:53 +0200

    start interpreter functies

--- tapecommander.py
+++ tapecommander.py
@@ -9,10 +9,10 @@ class TapeCommander:
 ##De volgorde lijkt omgekeerd dan op het scherm getoont wordt
 #wat je neit merkt als je alles gelijk inititaleert, en daarna werkt het oke
         self.tapes = [
-            t.Tape('ST', ['0','1','1','1'], 0),
-            t.Tape('RA', ['1','0','1','1'], 1),
-            t.Tape('RB', ['1','1','0','1'], 2),
-            t.Tape('S', ['1','1','1','0'], 3)]
+            t.Tape('ST', ['S','T','_','_'], 3),
+            t.Tape('RA', ['R','A','_','_'], 3),
+            t.Tape('RB', ['R','B','_','_'], 3),
+            t.Tape('S', ['S','_','_','_'], 3)]
 
     def getLabels(self):
         return self.TapeLabels
