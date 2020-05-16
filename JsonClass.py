a29ec31cad37e2a00ff0932c0a5d9836555493e5
janrutger <janrutger@jaruku.nl>
2020-05-16 18:22:04 +0200

    start interpreter functies

--- JsonClass.py
+++ JsonClass.py
@@ -12,125 +12,17 @@ class Opcodes:
 
     def __repr__(self):
         return f'<Opcodes { self.Name }>'
-
-
-        
-        
-        
-# ###        
-# json_string ="""[
-#   {
-#     "Name": "LDA",
-#     "States": {
-#       "S0": {
-#         "Rules": [
-#           {
-#             "MatchWaardes": {
-#               "ST": "1",
-#               "RA": "1",
-#               "RB": "1",
-#               "S": "1"
-#             },
-#             "NieuweWaardes": {
-#               "ST": "0",
-#               "RA": "0",
-#               "RB": "0",
-#               "S": "0"
-#             },
-#             "Move": {
-#               "ST": "L",
-#               "RA": "R",
-#               "RB": "L",
-#               "S": "S"
-#             },
-#             "NieuweStatus": "S0"
-#           },
-#           {
-#             "MatchWaardes": {
-#               "ST": "1",
-#               "RA": "1",
-#               "RB": "1",
-#               "S": "1"
-#             },
-#             "NieuweWaardes": {
-#               "ST": "0",
-#               "RA": "0",
-#               "RB": "0",
-#               "S": "0"
-#             },
-#             "Move": {
-#               "ST": "L",
-#               "RA": "R",
-#               "RB": "L",
-#               "S": "S"
-#             },
-#             "NieuweStatus": "S0"
-#           }
-#         ]
-#       },
-#       "S1": {
-#         "Rules": [
-#           {
-#             "MatchWaardes": {
-#               "ST": "1",
-#               "RA": "1",
-#               "RB": "1",
-#               "S": "1"
-#             },
-#             "NieuweWaardes": {
-#               "ST": "0",
-#               "RA": "0",
-#               "RB": "0",
-#               "S": "0"
-#             },
-#             "Move": {
-#               "ST": "L",
-#               "RA": "R",
-#               "RB": "L",
-#               "S": "S"
-#             },
-#             "NieuweStatus": "S0"
-#           },
-#           {
-#             "MatchWaardes": {
-#               "ST": "1",
-#               "RA": "1",
-#               "RB": "1",
-#               "S": "1"
-#             },
-#             "NieuweWaardes": {
-#               "ST": "0",
-#               "RA": "0",
-#               "RB": "0",
-#               "S": "0"
-#             },
-#             "Move": {
-#               "ST": "L",
-#               "RA": "R",
-#               "RB": "L",
-#               "S": "S"
-#             },
-#             "NieuweStatus": "S0"
-#           }
-#         ]
-#       }
-#     }
-#   }
-# ]"""
-
-# Opcodes = Opcodes.from_json(json_string)
-# print(Opcodes)
-# print(Opcodes.Name)
-# print(Opcodes.States)
+      
+    
 
 Opcodeslist = []
 with open('OpcodeData.json', 'r') as json_file:
-    opcodedata = json.loads(json_file.read())
+    opcodedata = json.load(json_file.read())
     for u in opcodedata:
         Opcodeslist.append(opcodedata[0])
 
 
 print("--------")
 print(Opcodeslist)
-print(Opcodeslist.Name)
+
 
