0fa3561f70365cb89bb53462852f057e054bec95
ernstvorsteveld <ernst.vorsteveld@gmail.com>
2020-05-21 20:24:35 +0200

    added beginning of a testcase

---
+++ test_interpreter.py
@@ -0,0 +1,19 @@
+import unittest
+import tape as tape
+import json
+import Opcode as o
+import state as s
+import interpreter as ip
+import Opcodeloader as ol
+
+class TestTape(unittest.TestCase):
+
+    def setUp(self):
+        with open("state-test-data.json", 'r') as json_file:
+            self.data = json.loads(json_file.read())
+
+    def test_load_opcode(self):
+        loader = ol.OpcodeLoader()
+        loader.load()
+        interpreter = ip.Interpreter(loader.get(), o.Opcode(**self.data), "")
+        print(interpreter)
