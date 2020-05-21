e08e341e14bbd082f2b24b6cf3560f8d52e6758e
janrutger <janrutger@jaruku.nl>
2020-05-21 20:05:03 +0200

    Interpreter poging 2

--- interpreter.py
+++ interpreter.py
@@ -15,36 +15,76 @@ import Opcode as oc
 class Interpreter():
     def __init__(self, opcodelibrary, opcode, operand):
         self.opcodelibrary = opcodelibrary
-        print(self.opcodelibrary, "----",  type(self.opcodelibrary))
+      #  print(self.opcodelibrary, "----",  type(self.opcodelibrary))
          
         self.opcode = opcode
         self.operand = operand       
         self.opcodeintern = ["PUSH", "TEST"] # Dit commando set de OPERAND op de stack, en kan niet door states worden afgehadeld
         print(self.opcodeintern, "----",  type(self.opcodeintern))
         self.check()
-        
+   
+    def contains(self, value):
+        print(value)
+        print(self.opcodelibrary, "----",  type(self.opcodelibrary))
+        i = 0
+        for item in self.opcodelibrary:
+            print(self.opcodelibrary[i])
+            if value == self.opcodelibrary[i]:
+                return True
+            i = i + 1
+        return False
+            
+   
 
     def check(self):
         if self.opcode in self.opcodeintern:
             print("Interne OPCODE", self.opcode)
-        elif any(opcodex == self.opcode for opcodex in self.opcodelibrary):
+        
+        elif self.contains(self.opcode) == True:
             print("JSON OPCODE", self.opcode)
- #       elif self.opcode in enumerate(self.opcodelibrary):
- #       elif self.opcode in self.opcodelibrary:           
+            
         else:
-            print("Onbekende opcode", self.opcode)
+            print("Onbekende opcode", self.opcode.__dict__)
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            #for waarde in self.opcodelibrary:
+                
+        
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+#print("Onbekende opcode", self.opcode)
  
  #if any(obj['shape'] == 'square' for obj in shapes):
-            
-#             for waarde in self.opcodelibrary:
-#                 if waarde == self.opcode:
-#                     print("JSON OPCODE", self.opcode)
-#                 
+           
+#              for waarde in self.opcodelibrary:
+#                  if waarde.name == self.opcode:
+#                      print("JSON OPCODE", self.opcode)
+# #                 
 #  
-#             
+#         if listOfStrings.count('at') > 0 :    
         
         
-       
+#elif any(opcodex == self.opcode for opcodex in self.opcodelibrary) == True:
+        #elif self.opcodelibrary.count(self.opcode) > 0:
+        
+#        elif self.opcode in enumerate(self.opcodelibrary):
+#        elif self.opcode in self.opcodelibrary:
+            #print("JSON OPCODE", self.opcode)      
             
 
          
