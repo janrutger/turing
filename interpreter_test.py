35a6b89266184c8577e8a32b2195feec4ee81ea6
janrutger <janrutger@jaruku.nl>
2020-05-22 18:30:54 +0200

    Interpreter poging 3

--- interpreter_test.py
+++ interpreter_test.py
@@ -8,21 +8,23 @@ import Opcodeloader as ol
 
 class TestInterpreter(unittest.TestCase):
 
-    def setUp(self):
-        with open("state-test-data.json", 'r') as json_file:
-            self.data = json.loads(json_file.read())
+#     def setUp(self):
+#         with open("state-test-data.json", 'r') as json_file:
+#             self.data = json.loads(json_file.read())
 
     def test_interpreter_opcode(self):
         loader = ol.OpcodeLoader()
         loader.load()
-        interpreter = ip.Interpreter(loader.get(), o.Opcode(**self.data), "")
-        # interpreter = ip.Interpreter(loader.get(), "LDA", "nog niet")
+        #interpreter = ip.Interpreter(loader.get(), o.Opcode(**self.data), "")
+        interpreter = ip.Interpreter(loader.get(), "MOVE", "nog niet")
         print(interpreter.opcode)
         print(interpreter.operand)
         print(interpreter.opcodelibrary)
         print(type(interpreter.opcodelibrary))
+        print(interpreter.contains(interpreter.opcode))
 
         self.assertEqual(interpreter.contains("LDA"), True)
+        self.assertEqual(interpreter.contains("dd"), False)
         #print(interpreter.opcodelibrary[1])
         
 #         if interpreter.opcode in interpreter.opcodelibrary:
