f9a0dca0ce2b51b6dcdebd810c2d59fbe9db0dae
janrutger <janrutger@jaruku.nl>
2020-05-24 12:31:54 +0200

    Start sqequencer 1

--- sequencer.py
+++ sequencer.py
@@ -4,8 +4,5 @@ import rule as rl
 class Sequencer():
     def __init__(self, statelibrary):
         self.statelibrary = statelibrary
-        self.currentstate = ["PUSH", "TEST"]
-        self.opcode = opcode
-        self.opcodestates = []         
-        self.operand = operand
+        self.currentstate = ["start"]
         self.check()
