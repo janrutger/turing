9bc7faba26020149036f78078d0a39e968cc5b3b
Janrutger <janrutger@jaruku.nl>
2020-11-20 11:50:12 +0100

    Start executer: P.O.C2

--- executer.py
+++ executer.py
@@ -7,30 +7,38 @@ class Executer:
     def __init__(self):
         #self.opcodes = oc.Opcodes() # verplaatst naar de exec_opcode class
         tapecommander = tc.Tapecommander()
-        self.no_opcodes = {"PUSH", "PULL"}
+        #self.no_opcodes = {"PUSH", "PULL"}
         self.execNOP = nop.Exec_no_opcode(tapecommander)
         self.execOP  = op.Exec_opcode(tapecommander)
 
 
-    def run_no_opcode(self, commando, operand):
+    def run_commando(self, commando, operand):
         if commando == "PUSH":
-            return(self.execNOP.push(operand))
+             return(self.execNOP.push(operand))
         if commando == "PULL":
-            return(self.execNOP.pull())
-        else:
-            return(-1)
-
-    def run_opcode(self, commando):
-        print(commando)
-        return(self.execOP.run(commando))
-
-
-    def run__commando(self, commando, operand):
-        if commando in self.no_opcodes:
-            return(self.run_no_opcode(commando, operand))
-            
+             return(self.execNOP.pull())
         else:
             return(self.run_opcode(commando))
+
+    # def run_no_opcode(self, commando, operand):
+    #     if commando == "PUSH":
+    #         return(self.execNOP.push(operand))
+    #     if commando == "PULL":
+    #         return(self.execNOP.pull())
+    #     else:
+    #         return(-1)
+
+    # def run_opcode(self, commando):
+    #     print(commando)
+    #     return(self.execOP.run(commando))
+
+
+    # def run__commando(self, commando, operand):
+    #     if commando in self.no_opcodes:
+    #         return(self.run_no_opcode(commando, operand))
+            
+    #     else:
+    #         return(self.run_opcode(commando))
             
             
     
