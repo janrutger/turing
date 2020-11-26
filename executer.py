72247e7031d450ac02edb9eeab0850862b27320d
Janrutger <janrutger@jaruku.nl>
2020-11-26 11:54:34 +0100

    executer: run_program

--- executer.py
+++ executer.py
@@ -20,4 +20,25 @@ class Executer:
         else:
             return(self.execOP.run(commando))
 
+    def run_program(self, program):
+        PC = 0
+
+        while PC < len(program):
+            programline = program[PC]
+            if len(tuple(programline)) == 2:
+                opcode  = programline[0]
+                operand = programline[1]
+                self.run_commando(opcode, operand)
+
+            if len(tuple(programline)) == 1:
+                opcode = programline[0]
+                self.run_commando(opcode, None)
+                
+            if len(tuple(programline)) == 0:
+                return("HALT")
+            PC = PC +1
+        else:
+            return("error")
+
+
    
--- test_executer.py
+++ test_executer.py
@@ -20,12 +20,6 @@ class test_executer(unittest.TestCase):
         result = self.executer.run_commando("PRINT", tapelist)
         self.assertEqual(result, {'ST': ['___#1011001#101', '1', '']})
 
-
-
-    def test_run_aOther_commando(self):
-        #self.executer.run_commando("PULL", None)
-        self.assertEqual(self.executer.run_commando("PULL", None), "0")
-
     def test_run_PUSH_LDA_opcodes(self):
         self.executer.run_commando("PUSH", "1101")
         self.executer.run_commando("PUSH", "0010")
@@ -38,6 +32,22 @@ class test_executer(unittest.TestCase):
         self.assertEqual(self.executer.run_commando("ADD", None), "HALT")
         self.executer.run_commando("PUSH", "1")
         self.assertEqual(self.executer.run_commando("ADD", None), "HALT")
+
+    def test_run_aOther_commando(self):
+        #self.executer.run_commando("PULL", None)
+        self.assertEqual(self.executer.run_commando("PULL", None), "0")
+
+    def test_executer_program(self):
+        program = [
+            ("PUSH", "101011"),
+            ("PUSH", "110101"),
+            ("LDA",),
+            ("ADD",),
+            ()
+        ]
+        #exitcode = self.executer.run_program(program)
+        #print(self.executer.run_program(program))
+        self.assertEqual(self.executer.run_program(program), "HALT")
 
 
 if __name__ == '__main__':
