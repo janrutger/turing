1b3e96cac45be4226e8c13b754954c0bc046e64c
Janrutger <janrutger@jaruku.nl>
2020-12-09 19:53:31 +0100

    MMU versie 1, more testing

--- test_mmu.py
+++ test_mmu.py
@@ -1,6 +1,6 @@
 import unittest
 from assembler import assembler as asm
-from mmu import mmu as mmu
+from mmu       import mmu       as mmu
 import executer as e
 
 
@@ -21,15 +21,18 @@ class test_opcodes(unittest.TestCase):
     def test_dump(self):
         self.memMgr.loadMem(self.BINprogram)
         memDump = self.memMgr.dumpMem()
-        self.assertEqual(memDump, [('PUSH', '1010')])
+        self.assertEqual(memDump, [('PUSH', '1010'), ('LDA', ''), ('PUSH', '1010')])
     
     def test_read(self):
         self.memMgr.loadMem(self.BINprogram)
-        memVal = self.memMgr.readMem(0)
+        memVal = self.memMgr.readMem(2)
         self.assertEqual(memVal, ('PUSH', '1010'))
 
     def test_write(self):
+    
         self.memMgr.loadMem(self.BINprogram)
+        print(self.memMgr.dumpMem())
+
         memVal = ('MEM', '1001')
         self.memMgr.writeMem("$jr", memVal)
         memVal = ('MEM', '1111')
