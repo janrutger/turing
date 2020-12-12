994cb484681f8366dc56baad60e2979808954dd3
Janrutger <janrutger@jaruku.nl>
2020-12-12 19:46:43 +0100

    Load from MEM store in MEM

--- assembler/jrk.asm
+++ assembler/jrk.asm
@@ -1,48 +1,22 @@
-push 0b1100
-push 0b1010
+# test program
 
-storem $jan1
-storem $jan
+	push 2
+	push 2
 
-loadm $jan1
-loadm $jan
-
-halt
-loadm  $jan
-halt
-
-# Dit is mijn eerste test programma
-	push 7
+	storem $getal
 	push 0
-
 	loada
-	loadb	
+	loadb
 
-:test
-	teste
-	jumpt :verder
-
-:plus
-	push 1
+:loop
+	loadm $getal
 	add
-	jump :test
 
-
-
-:verder
-	#ex
 	decrementb
-	ex
-	testz
-	ex 
-	jumpt :einde
-	jumpf :verder
+	jumpf :loop
 
-
-:einde
-	#push 0b1010
-	#ex
 	storea
-	halt
-	add
+	storem $result
+	loadm $result
+
 	halt
